7f88cbaf56bcea628388710616df7f2d36d3164c
adarench <adam.rencher12@gmail.com>
2024-09-18 17:48:06 -0600

    position methods

--- chess/shared/src/main/java/chess/ChessPosition.java
+++ chess/shared/src/main/java/chess/ChessPosition.java
@@ -7,23 +7,46 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessPosition {
-
-    public ChessPosition(int row, int col) {
+    private int row;
+    private int column;
+    public ChessPosition(int row, int column) {
+        this.row = row;
+        this.col = column;
     }
-
     /**
      * @return which row this position is in
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
-
+    public int getColumn(){
+        return column;
+    }
+    public void setRow(){
+        this.row = row;
+    }
+    public void setColumn(){
+        this.column = column;
+    }
     /**
      * @return which column this position is in
      * 1 codes for the left row
      */
-    public int getColumn() {
-        throw new RuntimeException("Not implemented");
+//equals method
+    @Override
+    public boolean equals(Object o){
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ChessPosition that = (ChessPosition) o;
+    return row == that.row && column == that.column;
     }
+
+    //hash cases
+    @Override
+    public int hashCode(){
+        return Objects.has(row, column);
+    }
+
 }
+
